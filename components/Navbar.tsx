577a930017a3aa3c294d13ff3c62579ca9f4104f
NeilRow56 <neilrowland56@gmail.com>
2023-08-29 07:57:25 +0100

    First commit- added dark mode and starter code

---
+++ components/Navbar.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link"
+import ThemeButton from "./ThemeButton"
+
+
+
+
+const Navbar = () => {
+ return (
+   <header className="py-6 bg-slate-200 dark:bg-slate-800 sticky top-0 z-[70]" >
+     <nav className="container mx-auto flex items-center justify-between px-48">
+       <ul className="flex gap-6">
+           <li>
+               <Link href='/'>Home</Link>
+           </li>
+       </ul>
+       <ThemeButton />
+     </nav>
+   </header>
+ )
+}
+
+
+export default Navbar
